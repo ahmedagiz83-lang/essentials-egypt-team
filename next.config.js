b035a49e8f8ed675bbf363a78fffa456944f48d1
ahmedagiz83-lang <ahmedagiz83@gmail.com>
2026-04-08 13:41:45 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { remotePatterns: [] },
-};
-module.exports = nextConfig;
